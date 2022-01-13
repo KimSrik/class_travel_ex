e43d3c3a1ddf19248de970f57b989f54a6bd25e6
KimSrik <95198474+KimSrik@users.noreply.github.com>
2022-01-13 18:12:11 +0900

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,22 @@
+
+// $(function(){
+//     $('#btn-nav').on({
+//         'click':function(){
+//               $('.header,.nav').toggleClass('show-nav');
+                
+//         },
+//     });
+// })
+
+
+
+window.onload=function(){
+    let btnNav = document.getElementById('btn-nav');
+    btnNav.addEventListener('click', function(){
+        let _header = document.querySelector('.header');
+        let _nav = document.querySelector('.nav');
+        
+        _header.classList.toggle('show-nav');
+        _nav.classList.toggle('show-nav');
+    });
+}
