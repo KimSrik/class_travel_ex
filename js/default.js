fa10d70c51dbec68cef005e303ffae77e9c1a687
KimSrik <95198474+KimSrik@users.noreply.github.com>
2022-01-14 15:03:16 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -19,4 +19,34 @@ window.onload=function(){
         _header.classList.toggle('show-nav');
         _nav.classList.toggle('show-nav');
     });
+
+    
+
+
+}
+// 슬라이더 생성 함수
+function mySlider(_target){
+    $(_target).bxSlider();
+}
+
+function disBlock(eventEle, target){
+    eventEle.addEventListener('click', function(){
+        target.style.display = 'block';
+    })
+    
+    
+}
+function disNone(eventEleTarget){
+    for(let idx=0; idx<eventEleTarget.length; idx++){
+        eventEleTarget[idx].addEventListener('click', function(){
+            eventEleTarget[idx].style.display = 'none';
+           
+        })
+    }
+}
+
+function smScl(_target){
+    $(_target).smoothScroll({
+        speed: 600,
+    })
 }
